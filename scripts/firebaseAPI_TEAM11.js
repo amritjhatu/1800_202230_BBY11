a6ea4004e4f230c8fd5faace4d925a33d4a12025
Amrit Jhatu <a_jhatu@hotmail.com>
2022-10-31 14:19:53 -0700

    Login Authentication and firebase libraries added

---
+++ scripts/firebaseAPI_TEAM11.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpLLGyCtmGhAxnikIWNy1LBTpF8oT1joQ",
+    authDomain: "comp1800-aa984.firebaseapp.com",
+    projectId: "comp1800-aa984",
+    storageBucket: "comp1800-aa984.appspot.com",
+    messagingSenderId: "774894479593",
+    appId: "1:774894479593:web:50dc6bfa0c1cd6642f6878"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
