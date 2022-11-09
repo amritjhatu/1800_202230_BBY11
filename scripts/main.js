0dacc2f17e1678300f66416197f02deb25ec7c7b
Greg Song <gregjsong@gmail.com>
2022-11-09 11:13:14 -0800

    changed main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,33 @@
-function insertName() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid);
-            console.log(user.displayName);
-            user_Name = user.displayName;
-
-            //method #1:  insert with html only
-            //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-            //method #2:  insert using jquery
-            $("#name-goes-here").text(user_Name); //using jquery
-
-        } else {
-            // No user is signed in.
-        }
-    });
+// Initialize and add the map
+function initMap() {
+  // Locations
+  /*
+   * This section will be populated by: 
+   * - user's coordinates for value of center in const map
+   * - markers within some proximity of center/user's coordinates
+   */
+  const bcit = { lat: 49.2490, lng: -123.0019 };
+  // The map, centered at BCIT for now - change to user's coordinates
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: bcit,
+  });
+  // The marker, positioned at BCIT - change later
+  const marker = new google.maps.Marker({
+    position: bcit,
+    map: map,
+  });
 }
-insertName(); //run the function
+
+window.initMap = initMap;
+
+// Load nav and footer html
+/**
+ * Maybe change this to not require JQuery and just make our own function to load text
+ * temp for now
+ */
+function loadSkeleton(){
+  console.log($('#navbar-container').load('../../text/nav.html'));
+  // console.log($('#footer-container').load('../../text/footer.html'));
+}
+loadSkeleton();
