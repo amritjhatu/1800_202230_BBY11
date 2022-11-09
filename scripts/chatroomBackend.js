052ebf268dc2f917a6dcccaf4738c4933628a191
kalel <kaleletendre@gmail.com>
2022-11-09 08:19:38 -0800

    updated backend

--- scripts/chatroomBackend.js
+++ scripts/chatroomBackend.js
@@ -9,36 +9,26 @@ class chatRoom {
             this.lat = lat;
             this.lng = lng;
     }
-    getChatRoom(roomId){
+    async getChatRoom(roomId){
         this.roomId = roomId
-        db.collection("Rooms").doc("BC").collection("room1").doc(roomId).get().then(somedoc => {                                                               //arrow notation
+        db.collection("Rooms").doc("BC").collection("room1").doc(roomId).onSnapshot(somedoc => {                                                               //arrow notation
             this.name = somedoc.data().name;
             this.lat = somedoc.data().lat;
             this.lng = somedoc.data().lng;
-            console.log(this.getName());
-            console.log(this.getLat());
-            console.log(this.getLng());
+            console.log(this.name);
         });
     }
     sendMessage(){
     //acess a sub directory in chatroom that contains messages, create new message and upload to database as newest message
     }
-    updateMessages(){
-    //acess a sub directory in chatroom that contains messages, return all the current mesages in chronological order
-    //set a cap on messages to display, as to not cause memory error
-    }
-    getName(){
-        return this.name;
-    }
-    getLat(){
-        return this.lat;
-    }
-    getLng(){
-        return this.lng;
+    async updateMessages() {
+        const snapshot = await db.collection("Rooms").doc("BC").collection("room1").doc(this.roomId).collection("messages").onSnapshot();
+        return snapshot.docs.map(doc => doc.data());
     }
 }
 // HOW TO CALL A CHAT ROOM WITH KNOW ROOM ID
 room  = new chatRoom();
-room.getChatRoom("pHBr8gfubDVWKv8GjE6o");
+console.log(room.getChatRoom("pHBr8gfubDVWKv8GjE6o"));
+
 
 
