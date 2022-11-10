84e19907fd51d14aa8f700397deec8220ff2c565
kaleLetendre <kaleletendre@gmail.com>
2022-11-09 21:20:32 -0800

    polishing chatroom functionality

--- scripts/chatroomBackend.js
+++ scripts/chatroomBackend.js
@@ -7,7 +7,7 @@ class chatRoom {
 
     async createChatroom(roomName,lati,long,province){
         var i = 0;
-        const snapshot = await db.collection(this.province).get()
+        const snapshot = await db.collection(province).get()
         snapshot.docs.map(doc =>{
             i++;
         });
@@ -15,7 +15,13 @@ class chatRoom {
             name: roomName,
             lat: lati,
             lng: long
-        });  
+        });
+        db.collection(province).doc("room"+i).collection("messages").add({
+            userEmail: "proxyChat",
+            order: 0,
+            message: "Welcome to the chat!"
+          })
+        
     }
     async accessChatroom(province,roomNumber,func){
         this.province = province;
@@ -53,22 +59,25 @@ class chatRoom {
 }
 // HOW TO CALL A CHAT ROOM WITH KNOW ROOM ID
 room  = new chatRoom();
-room.accessChatroom("BC","room1",function(){
-    console.log(room.name)
-    //This is where you can use the chatroom datails
-    //assign to your variable or use for whatever
-});
+// room.accessChatroom("BC","room1",function(){
+//     console.log(room.name)
+//     //This is where you can use the chatroom datails
+//     //assign to your variable or use for whatever
+// });
 
 //USE THIS TO UPDATE MEAGES FROM THE CHAT, WHICH CAN BE ACCESSED WITHIN THE FUNCTION BELOW
-room.updateMessages(function(){
-    console.log(room.messages);
-    //This is where you can use the messages
-    //assign to your variable or use for whatever
-});
+// room.updateMessages(function(){
+//     console.log(room.messages);
+//     //This is where you can use the messages
+//     //assign to your variable or use for whatever
+// });
 
 //USE THIS TO ADD A MESSAGE TO THE CHAT
 //room.sendMessage("i also also came from the client side :D","kaleletendre@gmail.com");
 
 //USE THIS TO CREAT A CHATROOM
-//room.createChatroom("fromClientSide",49.24,-123,"BC");
+room.createChatroom("SW05",49.24976319428734,-123.00258261514804,"BC");
+room.createChatroom("Metrotown Train",49.225855792181044,-123.00384054081385,"BC")
+room.createChatroom("Timmies",49.25035693390576,-123.00157940196397,"BC");
+room.createChatroom("Gym",49.24877976397611,-123.00091043350153,"BC")
 
