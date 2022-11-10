d1ae183be183498b4110cddc754f48f0737587e7
kaleLetendre <kaleletendre@gmail.com>
2022-11-09 20:11:31 -0800

    finished chatroom functionality :D

--- scripts/chatroomBackend.js
+++ scripts/chatroomBackend.js
@@ -1,35 +1,51 @@
 class chatRoom {
-    roomId;
+    roomNumber;
     name;
     lat;
     lng;
     messages = [];
 
-    createChatroom(name,lat,lng){
-            this.name = name;
-            this.lat = lat;
-            this.lng = lng;
+    async createChatroom(roomName,lati,long,province){
+        var i = 0;
+        const snapshot = await db.collection(this.province).get()
+        snapshot.docs.map(doc =>{
+            i++;
+        });
+        db.collection(province).doc("room"+i).set({
+            name: roomName,
+            lat: lati,
+            lng: long
+        });  
     }
-    async accessChatroom(roomId,province,roomName,func){
-        this.roomId = roomId
+    async accessChatroom(province,roomNumber,func){
         this.province = province;
-        this.roomName = roomName;
+        this.roomNumber = roomNumber;
 
-        db.collection("Rooms").doc(province).collection(roomName).doc(roomId).onSnapshot(somedoc =>{
+        db.collection(province).doc(roomNumber).onSnapshot(somedoc =>{
             this.name = somedoc.data().name;
             this.lat = somedoc.data().lat;
             this.lng = somedoc.data().lng;
             return func();
         });
     }
-    sendMessage(){
-    //acess a sub directory in chatroom that contains messages, create new message and upload to database as newest message
-    }
-    async updateMessages(roomId,province,roomName,func) {
+    async sendMessage(messageContent,email){
+        //acess a sub directory in chatroom that contains messages, create new message and upload to database as newest message
         var i = 0;
-        const snapshot = await db.collection("Rooms").doc(province).collection(roomName).doc(roomId).collection("messages").get()
+        const snapshot = await db.collection(this.province).doc(this.roomNumber).collection("messages").get()
+        snapshot.docs.map(doc =>{
+            i++;
+        });
+        db.collection(this.province).doc(this.roomNumber).collection("messages").add({
+            message: messageContent,
+            userEmail: email,
+            order: i
+        });  
+    }
+    async updateMessages(func) {
+        var i = 0;
+        const snapshot = await db.collection(this.province).doc(this.roomNumber).collection("messages").get()
         snapshot.docs.map(doc =>{ 
-            this.messages[i] = doc.data().message
+            this.messages[doc.data().order] = doc.data().message;
             i++;
         });
         return func();
@@ -37,13 +53,23 @@ class chatRoom {
 }
 // HOW TO CALL A CHAT ROOM WITH KNOW ROOM ID
 room  = new chatRoom();
-room.accessChatroom("pHBr8gfubDVWKv8GjE6o","BC","room1",function(){
+room.accessChatroom("BC","room1",function(){
     console.log(room.name)
     //This is where you can use the chatroom datails
     //assign to your variable or use for whatever
 });
-room.updateMessages(room.roomId,room.province,room.roomName,function(){
+
+//USE THIS TO UPDATE MEAGES FROM THE CHAT, WHICH CAN BE ACCESSED WITHIN THE FUNCTION BELOW
+room.updateMessages(function(){
     console.log(room.messages);
+    //This is where you can use the messages
+    //assign to your variable or use for whatever
 });
+
+//USE THIS TO ADD A MESSAGE TO THE CHAT
+//room.sendMessage("i also also came from the client side :D","kaleletendre@gmail.com");
+
+//USE THIS TO CREAT A CHATROOM
+room.createChatroom("fromClientSide",49.24,-123,"BC");
 
 
