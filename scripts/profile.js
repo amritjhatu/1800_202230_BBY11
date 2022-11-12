59bd5779b3a71cbc247e720eb28832e2de4261f8
Amrit Jhatu <a_jhatu@hotmail.com>
2022-11-12 02:54:38 -0800

    Profile stuff added. js needs to be linked to db.

---
+++ scripts/profile.js
@@ -0,0 +1,53 @@
+var currentUser          //put this right after you start script tag before writing any functions.
+
+function populateInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.email)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userName = userDoc.data().name;
+                    var userSchool = userDoc.data().school;
+                    var userCountry = userDoc.data().country;
+                    var userPicture = userDoc.data().picture;
+
+                    //if the data fields are not empty, then write them in to the form.
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userSchool != null) {
+                        document.getElementById("schoolInput").value = userSchool;
+                    }
+                    if (userCity != null) {
+                        document.getElementById("countryInput").value = userCity;
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log ("No user is signed in");
+        }
+    });
+}
+
+function showUploadedPicture(){
+    const fileInput = document.getElementById("mypic-input");   // pointer #1
+    const image = document.getElementById("mypic-goes-here");   // pointer #2
+
+    //attach listener to input file
+    //when this file changes, do something
+    fileInput.addEventListener('change', function(e){
+
+        //the change event returns a file "e.target.files[0]"
+        var blob = URL.createObjectURL(e.target.files[0]);
+
+        //change the DOM img element source to point to this file
+        image.src = blob;    //assign the "src" property of the "img" tag
+    })
+}
+showUploadedPicture();
+// I (Amrit) need to work and fix the profile upload stuff and store it
