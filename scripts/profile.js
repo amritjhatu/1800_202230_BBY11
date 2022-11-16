270d2e3a2f3a0bef98847ebc332ea4a42672638d
Amrit Jhatu <a_jhatu@hotmail.com>
2022-11-16 00:11:39 -0800

    Profile adjusted. Requires Storage Connection

--- scripts/profile.js
+++ scripts/profile.js
@@ -23,8 +23,11 @@ function populateInfo() {
                     if (userSchool != null) {
                         document.getElementById("schoolInput").value = userSchool;
                     }
-                    if (userCity != null) {
-                        document.getElementById("countryInput").value = userCity;
+                    if (userCountry != null) {
+                        document.getElementById("countryInput").value = userCountry;
+                    }
+                    if (userPicture != null) {
+                        document.getElementById("pictureInput").value = userPicture;
                     }
                 })
         } else {
@@ -34,8 +37,34 @@ function populateInfo() {
     });
 }
 
+//call the function to run it 
+populateInfo();
+
+function editUserInfo() {
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
+ }
+
+ function saveUserInfo() {
+  var  userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+  var  userSchool = document.getElementById('schoolInput').value;     //get the value of the field with id="schoolInput"
+  var  userCountry = document.getElementById('countryInput').value;       //get the value of the field with id="countryInput"
+  var  userPicture = document.getElementById('pictureInput').value;
+
+  currentUser.update({
+    name: userName,
+    school: userSchool,
+    country: userCountry,
+    picture: userPicture
+})
+.then(() => {
+    console.log("Document successfully updated!");
+})
+    document.getElementById('personalInfoFields').disabled = true;
+}
+
 function showUploadedPicture(){
-    const fileInput = document.getElementById("mypic-input");   // pointer #1
+    const fileInput = document.getElementById("pictureInput");   // pointer #1
     const image = document.getElementById("mypic-goes-here");   // pointer #2
 
     //attach listener to input file
@@ -50,4 +79,62 @@ function showUploadedPicture(){
     })
 }
 showUploadedPicture();
-// I (Amrit) need to work and fix the profile upload stuff and store it
+
+function uploadUserProfilePic() {
+    // Let's assume my storage is only enabled for authenticated users 
+    // This is set in your firebase console storage "rules" tab
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        var fileInput = document.getElementById("pictureInput");   // pointer #1
+        const image = document.getElementById("mypic-goes-here"); // pointer #2
+
+        // listen for file selection
+        fileInput.addEventListener('change', function (e) {
+            var file = e.target.files[0];
+            var blob = URL.createObjectURL(file);
+            image.src = blob;            // display this image
+
+            //store using this name
+            var storageRef = storage.ref("images/" + user.uid + ".jpg"); 
+            
+            //upload the picked file
+            storageRef.put(file) 
+                .then(function(){
+                    console.log('Uploaded to Cloud Storage.');
+                })
+
+						//get the URL of stored file
+            storageRef.getDownloadURL()
+                .then(function (url) {   // Get URL of the uploaded file
+                    console.log(url);    // Save the URL into users collection
+                    db.collection("users").doc(user.email).update({
+                        "picture": url
+                    })
+                    .then(function(){
+                        console.log('Added Profile Pic URL to Firestore.');
+                    })
+                })
+        })
+    })
+}
+uploadUserProfilePic();
+
+function displayUserProfilePic() {
+    console.log("hi");
+    firebase.auth().onAuthStateChanged(function (user) {      //get user object
+        db.collection("users").doc(user.email)                  //use user's uid
+            .get()                                            //READ the doc
+            .then(function (doc) {
+                var picUrl = doc.data().picture;           //extract pic url
+
+								// use this line if "mypicdiv" is a "div"
+                //$("#mypicdiv").append("<img src='" + picUrl + "'>")
+                
+								// use this line if "mypic-goes-here" is an "img" 
+								$("#mypic-goes-here").attr("src", picUrl);
+            })
+    })
+}
+displayUserProfilePic();
+
+
